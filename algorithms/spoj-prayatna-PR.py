d1cbda6ca1ada7729a3cff2f412f1331b54d9849
Steven Nguyen <steven.nguyenhung@gmail.com>
2018-06-21 00:12:57 +0800

    DFS one

---
+++ algorithms/spoj-prayatna-PR.py
@@ -0,0 +1,74 @@
+'''
+' Steven June 19 2018
+' https://www.spoj.com/problems/CAM5/
+' BFS, DFS
+'''
+
+
+def readInput():
+  line = input()
+  while not line.strip():
+    line = input()
+  return line
+
+
+def dfs(graph, s, visited):
+  # Queue
+  st = [s]
+
+  visited[s] = True
+
+  while len(st) != 0:
+    u = st.pop()
+    for v in graph[u]:
+      if not visited[v]:
+        visited[v] = True
+        st.append(v)
+
+
+def bfs(graph, s, visited):
+  # Queue
+  q = [s]
+
+  visited[s] = True
+
+  while len(q) != 0:
+    nextLevel = []
+    for u in q:
+      for v in graph[u]:
+        if not visited[v]:
+          visited[v] = True
+          nextLevel.append(v)
+
+    q = nextLevel
+
+
+def solve():
+  # Number of peers
+  n = int(readInput())
+
+  # Number of edges
+  e = int(readInput())
+
+  # Build graph
+  graph = [[] for i in range(n)]
+  for i in range(e):
+    u, v = map(int, readInput().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+  # BFS
+  visited = [False]*n
+  counter = 0
+  for i in range(n):
+    if not visited[i]:
+      dfs(graph, i, visited)
+      counter += 1
+  print(counter)
+
+
+# test cases
+t = int(readInput())
+
+for i in range(t):
+  solve()
