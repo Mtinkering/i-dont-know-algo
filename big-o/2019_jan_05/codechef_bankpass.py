b406e17f79ebb5c5e415756d3246d0a522d75886
Steven Nguyen <steven.nguyenhung@gmail.com>
2019-01-14 02:44:08 +0800

    14 jan 2019

--- big-o/2019_jan_05/codechef_bankpass.py
+++ big-o/2019_jan_05/codechef_bankpass.py
@@ -2,57 +2,14 @@
 ##################
 # Correct        #
 ##################
-# class Node():
-#   def __init__(self):
-#     self.children = {}
-#     self.endOfWord = False
-
-# # True means there is prefix
-# def addPass(trie, pw):
-#   tmp = trie
-#   oldPath = True
-
-#   for c in pw:
-#     if c not in tmp.children:
-#       tmp.children[c] = Node()
-#       oldPath = False
-
-#     tmp = tmp.children[c]
-
-#     if tmp.endOfWord == True:
-#       return True
-
-#   tmp.endOfWord = True
 
 
-#   return oldPath
-
-# # if __name__ == "__main__":
-# n = int(input())
-
-# trie = Node()
-
-# ans = 'non vulnerable'
-
-# for i in range(n):
-#   pw = input()
-
-#   res = addPass(trie, pw)
-
-#   if res == True:
-#     ans = 'vulnerable'
-#     break
-
-# print(ans)
-
-
-##################
-# Wrong          #
-##################
 class Node():
   def __init__(self):
     self.children = {}
     self.endOfWord = False
+
+# True means there is prefix
 
 
 def addPass(trie, pw):
@@ -62,12 +19,12 @@ def addPass(trie, pw):
   for c in pw:
     if c not in tmp.children:
       tmp.children[c] = Node()
-    #   oldPath = False
+      oldPath = False
 
     tmp = tmp.children[c]
 
     if tmp.endOfWord == True:
-      return False
+      return True
 
   tmp.endOfWord = True
 
@@ -86,8 +43,56 @@ for i in range(n):
 
   res = addPass(trie, pw)
 
-  if res == False:
+  if res == True:
     ans = 'vulnerable'
+    print(pw)
     break
 
 print(ans)
+
+
+##################
+# Wrong          #
+##################
+# class Node():
+#   def __init__(self):
+#     self.children = {}
+#     self.endOfWord = False
+
+
+# def addPass(trie, pw):
+#   tmp = trie
+#   oldPath = True
+
+#   for c in pw:
+#     if c not in tmp.children:
+#       tmp.children[c] = Node()
+#     #   oldPath = False
+
+#     tmp = tmp.children[c]
+
+#     if tmp.endOfWord == True:
+#       return False
+
+#   tmp.endOfWord = True
+
+#   return oldPath
+
+
+# # if __name__ == "__main__":
+# n = int(input())
+
+# trie = Node()
+
+# ans = 'non vulnerable'
+
+# for i in range(n):
+#   pw = input()
+
+#   res = addPass(trie, pw)
+
+#   if res == False:
+#     ans = 'vulnerable'
+#     break
+
+# print(ans)
--- hack.py
+++ hack.py
@@ -1,41 +1,178 @@
-words = ['abd', 'abc', 'efg']
-
-words.sort()
-print(*words)
 
 
-class Node():
+class Node:
   def __init__(self):
+    self.endWord = False
     self.children = {}
-    self.endOfWord = False
 
-# True means there is prefix
+# add(1) # 0
+# add(2) # 10
+# add(3) # 110
+
+# add(2) # 00
+# add(2) # 01
+# add(2) # 0
+# YES
+# 0000
+# 0001
+# 0010
+# 0011
+# 0100
+# 0101
+# 0110
+# 0111
+# 1000
+# 1001
+
+# 0000
+# 0001
+# 0010
+# 0011
+# ...
 
 
-def addPass(trie, pw):
-  tmp = trie
+def addNode(root, num, s, changed):  # num = 1, 2 ,3
+  if root.endWord == True:
+    return ''
 
-  for c in pw:
-    if c not in tmp.children:
-      tmp.children[c] = Node()
-    tmp = tmp.children[c]
-  tmp.endOfWord = True
+  if num == 0:
+    root.endWord = True
+
+    if changed == False:
+      return ''
+    return s
+
+  cur = root
+
+  c = '0'
+
+  if c not in cur.children:
+    #  or cur.children[c].endWord == False:
+    cur.children[c] = Node()
+    changed = True
+    return addNode(cur.children[c], num - 1, s + c, changed)
+
+    #   c = '1'
+
+    #   if c not in cur.children:
+
+    #     temp = addNode(cur.children[c], num - 1, s + c)
+
+    #     if temp == '':
+    #       return ''
+  else:
+    temp = addNode(cur.children[c], num - 1, s + c, changed)
+
+    if temp == '':
+      c = '1'
+
+      if c not in cur.children:
+        cur.children[c] = Node()
+        changed = True
+        return addNode(cur.children[c], num - 1, s + c, changed)
+
+      else:
+        temp = addNode(cur.children[c], num - 1, s + c, changed)
+
+        if temp == '':
+          return ''
+        else:
+          return temp
+
+    else:
+      return temp
+
+  # elif cur.children[c].endWord == True:
+  #   c = '1'
+
+  #     cur.children[c] = Node()
+
+  #   elif cur.children[c].endWord == True:
+  #     return ''
+
+  # cur = cur.children[c]
+
+  # cur.endWord = True
+
+  return s
+
+# def main():
+#   n = int(input())
+
+#   nums = map(int, input().split())
+#   res = []
+
+#   root = Node()
+#   # print(res)
+
+#   for num in nums:
+
+#     s = addNode(root, num, '')
+#     # print(s)
+#     if s == '':
+#       return []
+
+#     res.append(s)
+#   return res
 
 
-def inOrderTraversal(root, s):
-  if root.endOfWord == True:
-    print(s)
+def main():
+  n = int(input())
 
-  keys = root.children.keys()
+  nums = list(map(int, input().split()))
 
-  for key in sorted(keys):
-    inOrderTraversal(root.children[key], s + key)
+  res = []
+  words = {}
+  root = Node()
+  for num in nums:
+    words[num] = words.get(num, 0) + 1
+
+    if words[num] > 2**(num):
+      return []
+
+  for num in nums:
+    # print(num)
+    s = addNode(root, num, '', False)
+    res.append(s)
+
+  return res
 
 
-trie = Node()
+ans = main()
+
+if len(ans) == 0:
+  print('NO')
+else:
+  print('YES')
+  print('\n'.join(ans))
 
 
-for word in words:
-  addPass(trie, word)
+# def main():
+#   n = int(input())
 
-inOrderTraversal(trie, '')
+#   nums = map(int, input().split())
+
+#   res = []
+#   words = {}
+
+#   for num in nums:
+#     words[num] = words.get(num, 0) + 1
+
+#     if words[num] > 2**(num):
+#       return []
+
+
+#   for num in nums:
+#     s = addNode(root, num, '')
+
+#     res.append(s)
+
+#   return res
+
+# ans = main()
+
+# if len(ans) == 0:
+#   print('NO')
+# else:
+#   print('YES')
+#   print('\n'.join(ans))
