1bf4206d22fe488777ae668228c36b49753e8864
Steven Nguyen <steven.nguyenhung@gmail.com>
2019-04-27 20:10:28 +0800

    Topo sort

--- big-o/21-topological/topo-algo.py
+++ big-o/21-topological/topo-algo.py
@@ -1,4 +1,19 @@
+# 3 colors of a node:
+# RED: Visited
+# YELLOW: VISTING
+# GREEN: Not Visited
+
+# One important thing in topo sort is: If we care about the order of the output, for example, if a and b have no relation,
+# the order should be the same as input, then using kahn with priority queue is easier
+# But then the complexity is higher, due to maintaining the PQ
+
+# Using # 0: not visited. 1: visiting. 2: visited
+# visited = [0]*n
+# This allows to check if there is a cycle
+
+
 def dfs(u, graph, visited, result):
+  visited[u] = True
   for v in graph[u]:
     if not visited[v]:
       dfs(v, graph, visited, result)
